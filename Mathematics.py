00a1ed9e5bca6b8ac8cb2994d3d4e5afc17a5f19
karey-254 <pearlwangari@gmail.com>
2025-12-27 12:16:58 +0300

    Maths operations

---
+++ Mathematics.py
@@ -0,0 +1,13 @@
+my_int_1 = 56
+my_int_2 = 12
+
+my_float_1 = 5.4
+my_float_2 = 12.0
+
+mod_ints = my_int_1 % my_int_2
+mod_floats = my_float_2 % my_float_1
+print('modular integers:',  mod_ints ) # divides two numbers from the left to right
+print('modular float:', mod_floats)
+print('answer:', mod_ints * mod_floats)
+print('Exponential:',mod_ints ** mod_floats ) 
+print('floor:', mod_floats // mod_ints) # gives an integer equal to or less than the highest integrer
