e14e61edee9752b576965fdb594d5c2f89fde0fc
karey-254 <pearlwangari@gmail.com>
2025-12-27 12:58:30 +0300

    augumentation

--- Mathematics.py
+++ Mathematics.py
@@ -11,6 +11,8 @@ print('modular float:', mod_floats)
 print('answer:', mod_ints * mod_floats)
 print('Exponential:',mod_ints ** mod_floats ) 
 print('floor:', mod_floats // mod_ints) # gives an integer equal to or less than the highest integrer
+
+# Problem 2
 my_int_1 = 56
 my_int_2 = 12
 
@@ -23,3 +25,18 @@ print('exponential integer:',exp_ints)
 print('exponential float:',exp_floats)
 print('floor int:',my_int_1 //my_int_2) #floor it
 print('Modulo int:', my_int_1 % my_int_2) #Modulo it
+
+# Problem 3 augemented
+my_var = 10
+my_var += 5
+print(my_var)
+print(my_var + 10)
+print(my_var / 5)
+
+# Problem 4 augemented
+my_var = 113
+my_var_1 = 10
+print(my_var)
+print(my_var + 10)
+print(my_var / 5)
+print(my_var // my_var_1) #flooring
