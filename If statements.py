6909bc4d8641b981147016d45a4241240f061830
karey-254 <pearlwangari@gmail.com>
2025-12-28 14:27:33 +0300

    elif

--- If statements.py
+++ If statements.py
@@ -5,3 +5,20 @@ if age >= 18:
     print('You are an adult')
 else:
     print('You are not an adult yet')
+
+    #multiple conditions
+age = 2
+
+if age >= 65:
+    print('You are a senior citizen')
+elif age >= 30:
+    print('You are an adult in your prime')
+elif age >= 18:
+    print('You are a young adult')
+elif age >= 13:
+    print('You are a teenager')
+elif age >= 3:
+    print('You are a young child')
+else:
+    print('You are a toddler or an infant')
+
