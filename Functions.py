524dbf51bb8402ef5f86309445a6dcce7f94ab43
karey-254 <pearlwangari@gmail.com>
2025-12-29 10:01:07 +0300

    Funtion of return in Def

--- Functions.py
+++ Functions.py
@@ -1,3 +1,10 @@
 # input function
 name = input('What is your name?') # User types "Kolade" and presses Enter  
 print('Hello', name) 
+
+# Def function with the use of return
+def calculate_sum(a, b):
+    return a + b
+
+my_sum = calculate_sum(13, 21)
+print(my_sum)
