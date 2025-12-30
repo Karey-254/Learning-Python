b53fb0d980467e9743faefe96b041716edd5d793
karey-254 <pearlwangari@gmail.com>
2025-12-30 15:01:08 +0300

    Enclosing scope

--- LEGB.py
+++ LEGB.py
@@ -1,9 +1,20 @@
 #Local scope
 
-def my_func():
-    my_var = 10 # Locally scoped to my_func
-    print(my_var)
+#def my_func():
+    #my_var = 10 # Locally scoped to my_func
+    #print(my_var)
 
-my_func() # 10
+#my_func() # 10
 
-print(my_var) # NameError: name 'my_var' is not defined
+#print(my_var) # NameError: name 'my_var' is not defined
+
+#Enclosing scope
+def outer_func():
+    msg = 'Hello karey!'
+
+    def inner_func():
+        print(msg)
+
+    inner_func()
+
+outer_func() # Hello !Karey
