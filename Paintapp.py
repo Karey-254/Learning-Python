3944beac89b22a2aa69e8a7d1372337dba7963a3
karey-254 <pearlwangari@gmail.com>
2026-01-02 23:01:26 +0300

    I added a comment on color App

--- Paintapp.py
+++ Paintapp.py
@@ -1,4 +1,17 @@
 #coloring app
 pos_1 = (20, 400)
 col_1 = (255,0,0)
-circle_radius = (200)
+circle_radius = (200)#
+
+#Continue the color App
+circle_radius = 300
+
+pos_1 = (400, 20)
+col_1 = (255, 0, 0)
+
+pos_2 = (100, 200)
+col_2 = (40, 0, 0)
+
+
+pos_3 = (30, 150)
+col_3 = (70, 0, 0)
--- Variable.py
+++ Variable.py
@@ -1,3 +1,9 @@
 string_1 = 'I am a string'
 string_2 = "I am also a string"
 alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+#Review
+string_3 = 'Hello Wangari'
+ 
+Print (string_3)
+
