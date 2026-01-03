1a3ced83311c8d72bb5b190aa5214a1d2a1c4320
karey-254 <pearlwangari@gmail.com>
2026-01-03 21:05:29 +0300

    More painting

--- Paintapp.py
+++ Paintapp.py
@@ -15,3 +15,27 @@ col_2 = (40, 0, 0)
 
 pos_3 = (30, 150)
 col_3 = (70, 0, 0)
+
+
+pos_4 = (600, 70)
+col_4 = (100, 0, 0)
+
+my_image = "elephant.webp"
+
+def setup():
+
+    add_background(117, 52, 87,125 )
+
+def draw():
+
+    if is_drawing():
+
+        fill(1, 54, 69)
+        circle(mouse_x, mouse_y, 700)
+
+
+
+
+
+
+
