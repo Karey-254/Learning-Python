b1749b64b5cdbcef8e0f03a473b02ad5f507749f
karey-254 <pearlwangari@gmail.com>
2026-01-04 18:55:34 +0300

    Emojis

---
+++ Emoji.py
@@ -0,0 +1,21 @@
+# imports
+
+# variables
+world = '🌍🌎🌏'
+python = 'Python 🐍'
+fire = '🔥'
+
+# Function definitions        
+    
+# Put code to run under here
+
+print(f'Hello{world}')
+print(f'Welcome to {python}')
+print(f'{python} is good at Maths!')
+print(f'{111111111 * 111111111}')
+
+#imports
+from datetime import datetime
+
+# variables
+print(f'The date and time is {datetime.now()}')
